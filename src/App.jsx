2a95d91269ff3e2d150d90f2c0f901940038ac21
woodyjohnson13 <johnsonwoody13@gmail.com>
2023-07-10 21:03:54 +0400

    navbar ready

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ import {About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} f
 
 
 
-import './App.css'
 
 const App = () => {
 
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-
          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
             <Navbar/>
             <Hero/>
