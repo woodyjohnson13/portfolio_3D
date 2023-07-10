3eaac21f89b1c324dce3c43333a1c0b6a9c3da16
woodyjohnson13 <johnsonwoody13@gmail.com>
2023-07-10 21:43:03 +0400

    start adding computer canvas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
             <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
             <p className='text-white text-[18px] font-bold cursor-pointer flex '>
               Tim &nbsp;
-              <span className='sm:block hidden'> | Web Developer</span>
+              <span className='sm:block hidden'> | Enthusiast  Developer</span>
             </p>
           </Link>
 
